11b5b1b3372990bcd9879bbf496661bec6ccec24
Валерий <Waliush@mail.ru>
2022-06-29 13:32:20 +0300

    Продолжение работы с меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('.header__menu--burger').on('click', function (e) {
    e.preventDefault;
    $(this).toggleClass('header__menu--burger-active');
-   $('.header__menu--item').toggleClass('header__menu--item-active');
+   $('.header__menu--item').toggleClass('active');
 
 });
